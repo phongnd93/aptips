bab6a1a703c58ac7429fb95747cde855ef5b7a09
Phong <phongnd1@vbd.local>
2024-07-17 20:59:37 +0700

    fix(createAvatar)

--- src/utils/createAvatar.ts
+++ src/utils/createAvatar.ts
@@ -2,15 +2,17 @@
 
 const PRIMARY_NAME = ['A', 'N', 'H', 'L', 'Q', '9', '8'];
 const INFO_NAME = ['F', 'G', 'T', 'I', 'J', '1', '2', '3'];
-const SUCCESS_NAME = ['K', 'D', 'Y', 'B', 'O', '4', '5'];
+const SUCCESS_NAME = ['K', 'D', 'Y', 'B', 'O', '4', '5', '0'];
 const WARNING_NAME = ['P', 'E', 'R', 'S', 'C', 'U', '6', '7'];
 const ERROR_NAME = ['V', 'W', 'X', 'M', 'Z'];
 
-function getFirstCharacter(name: string) {
+function getFirstCharacter(name: string)
+{
   return name && name.charAt(0).toUpperCase();
 }
 
-function getAvatarColor(name: string) {
+function getAvatarColor(name: string)
+{
   if (PRIMARY_NAME.includes(getFirstCharacter(name))) return 'primary';
   if (INFO_NAME.includes(getFirstCharacter(name))) return 'info';
   if (SUCCESS_NAME.includes(getFirstCharacter(name))) return 'success';
@@ -19,7 +21,8 @@ function getAvatarColor(name: string) {
   return 'default';
 }
 
-export default function createAvatar(name: string) {
+export default function createAvatar(name: string)
+{
   return {
     name: getFirstCharacter(name),
     color: getAvatarColor(name),
