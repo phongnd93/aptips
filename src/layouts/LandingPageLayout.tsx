b3c47a902e3e95e042b2331242017f33300e75dd
dieptuha10 <107830953+dieptuha10@users.noreply.github.com>
2024-07-17 20:59:37 +0700

    feat: animate landing page

--- src/layouts/LandingPageLayout.tsx
+++ src/layouts/LandingPageLayout.tsx
@@ -1,14 +1,99 @@
-import { Stack } from '@mui/material';
+import { styled, useTheme } from '@mui/material/styles'; 
+import { AppBar, Toolbar, Container, Stack, Typography, Button } from '@mui/material';
 import { ReactNode } from 'react'
+import { HEADER } from 'src/config';
+import useResponsive from 'src/hooks/useResponsive';
+import cssStyles from 'src/utils/cssStyles';
+import Image from 'src/components/Image';
+import Link from 'next/link';
 
 type Props = {
     children: ReactNode
 }
 
-export default function LandingPageLayout({ children }: Props)
+const ToolbarStyle = styled(Toolbar)(({ theme }) => ({
+    height: HEADER.MOBILE_HEIGHT,
+    transition: theme.transitions.create(['height', 'background-color'], {
+        easing: theme.transitions.easing.easeInOut,
+        duration: theme.transitions.duration.shorter,
+    }),
+    [theme.breakpoints.up('md')]: {
+        height: HEADER.MAIN_DESKTOP_HEIGHT,
+    },
+}));
+
+const ToolbarShadowStyle = styled('div')(({ theme }) => ({
+    left: 0,
+    right: 0,
+    bottom: 0,
+    height: 24,
+    zIndex: -1,
+    margin: 'auto',
+    borderRadius: '50%',
+    position: 'absolute',
+    width: `calc(100% - 50px)`,
+    // boxShadow: theme.customShadows.z8,
+}));
+
+const MainHeader: React.FC = () =>
 {
+    const isOffset = true;
+
+    const theme = useTheme();
+
+    const isDesktop = useResponsive('up', 'md');
+
+    return (
+        <AppBar sx={{ boxShadow: 0, bgcolor: 'transparent' }}>
+            <ToolbarStyle
+                disableGutters
+                sx={{
+                    ...(isOffset && {
+                      ...cssStyles(theme).bgBlur(),
+                      height: { md: HEADER.MAIN_DESKTOP_HEIGHT - 16 },
+                    }),
+                    backgroundColor: 'rgba(0, 189, 214, 0.07)',
+                }}
+            >
+                <Container
+                    sx={{
+                        display: 'flex',
+                        alignItems: 'center',
+                        justifyContent: 'space-between',
+                    }}
+                >
+                    <Stack direction={'row'} alignItems={'center'} gap={2}>
+                        <Image
+                            sx={{ zIndex: 2, height: 60 , width: 60 }}
+                            src="/imgs/SuiCup_Logo_2.png"
+                            alt="suicuplogo2"
+                            visibleByDefault
+                            disabledEffect
+                        />
+                        <Stack>
+                            <Typography variant="h5" color={'#00BDD6'}>SUICUP</Typography>
+                            <Typography color={'black'}>Sui Me a Coffee</Typography>
+                        </Stack>
+                    </Stack>
+                    <Link
+                        href={'/auth/login'}
+                    >
+                        <Button variant='contained' size='medium' sx={{ borderRadius: '1.25rem', width: '100px' }}>
+                            Login
+                        </Button>
+                    </Link>
+                </Container>
+            </ToolbarStyle>
+            {isOffset && <ToolbarShadowStyle />}
+        </AppBar>
+    );
+}
+
+export default function LandingPageLayout({ children }: Props) {
     return (
         <Stack sx={{ minHeight: 1 }}>
+            <MainHeader />
+            {children}
         </Stack>
     );
 }
--- src/pages/landing-page.tsx
+++ src/pages/landing-page.tsx
@@ -1,3 +1,4 @@
+import { m } from 'framer-motion';
 import { Button, Stack, Typography } from "@mui/material";
 import { Container, Grid, Paper, styled } from "@mui/material";
 import Link from "next/link";
@@ -5,6 +6,7 @@ import Image from "src/components/Image";
 import Page from "src/components/Page";
 import useSettings from "src/hooks/useSettings";
 import Layout from "src/layouts";
+import { varFade, MotionViewport } from 'src/components/animate';
 
 
 const RootStyle = styled('div')(({ theme }) => ({
@@ -16,78 +18,93 @@ const RootStyle = styled('div')(({ theme }) => ({
 
 LandingPage.getLayout = function getLayout(page: React.ReactElement)
 {
-    return <Layout variant="logoOnly">{page}</Layout>;
+    return <Layout variant="landingPage">{page}</Layout>;
 };
 
 const LadingPageComponent = () =>
 {
     return (
-        <Grid container spacing={0}>
+        <Grid container spacing={3}>
             <Grid item xs={12}>
-                <Paper sx={{ px: 30, pt: 15 , pb: 5, bgcolor: 'rgba(0, 189, 214, 0.07)' }}>
-                    <Stack spacing={3} alignItems={'center'}>
-                        <Typography variant="h1" fontWeight={'700'}>
-                            Fund your creative work on <span style={{ color: '#00BDD6' }}>web3</span>
-                            </Typography>   
-                        <Typography fontSize={32} fontWeight={'400'}>
-                            Transparently, securely receive support from your fan
-                        </Typography>
-                        <Link href={'/'}>
-                            <Button
-                                variant="contained"
-                                size="large"
-                                sx={{ borderRadius: '1.5rem', maxWidth: 300, minWidth: 250 }}
-                            >
-                                Create Support
-                            </Button>
-                        </Link>
-                        <Typography fontSize={24}>It's free and takes less than a minute</Typography>
-                    </Stack>
+                <Paper sx={{ px: 30, pt: 10 , pb: 5, bgcolor: 'rgba(0, 189, 214, 0.07)', borderTopLeftRadius: 0, borderTopRightRadius: 0 }}>
+                    <m.div variants={varFade().inRight}>
+                        <Stack spacing={3} alignItems={'center'}>
+                            <Typography variant="h1" fontWeight={'700'}>
+                                Fund your creative work on <span style={{ color: '#00BDD6' }}>web3</span>
+                                </Typography>   
+                            <Typography fontSize={32} fontWeight={'400'}>
+                                Transparently, securely receive support from your fan
+                            </Typography>
+                            <Link href={'/'}>
+                                <Button
+                                    variant="contained"
+                                    size="large"
+                                    sx={{ borderRadius: '1.5rem', maxWidth: 300, minWidth: 250 }}
+                                >
+                                    Create Support
+                                </Button>
+                            </Link>
+                            <Typography fontSize={24}>It's free and takes less than a minute</Typography>
+                        </Stack>
+                    </m.div>
                 </Paper>
             </Grid>
             <Grid item xs={12}>
                 <Paper sx={{ px: 10, py: 10, bgcolor: 'white' }}>
                     <Stack spacing={0} alignItems={'center'}>
-                        <Image
-                            visibleByDefault
-                            disabledEffect
-                            sx={{ zIndex: 2, height: 513 , width: 574 }}
-                            src="/imgs/example_config.png"
-                            alt="example config"
-                        />
+                        <m.div variants={varFade().inUp}>
+                            <Image
+                                visibleByDefault
+                                disabledEffect
+                                sx={{ zIndex: 2, height: 513 , width: 574 }}
+                                src="/imgs/example_config.png"
+                                alt="example config"
+                            />
+                        </m.div>
                     </Stack>
                 </Paper>
             </Grid>
             <Grid item xs={12}>
                 <Paper sx={{ px: 31.5, py: 5, bgcolor: 'rgba(0, 189, 214, 0.07)' }}>
                     <Stack spacing={5} alignItems={'center'}>
-                        <Typography variant="h2" fontWeight={'700'}>
-                            Easy way to say thanks from your fans
-                        </Typography>   
-                        <Typography fontSize={24} fontWeight={'500'}>
-                            <span style={{ color: '#00BDD6'}}>Sui me a coffee</span> makes a easy and funny way,<br /> like receiving a cup of coffee from people loving you.
-                        </Typography>
+                        <m.div variants={varFade().inUp}>
+                            <Typography variant="h2" fontWeight={'700'}>
+                                Easy way to say thanks from your fans
+                            </Typography>   
+                            <Typography fontSize={24} fontWeight={'500'}>
+                                <span style={{ color: '#00BDD6'}}>Sui me a coffee</span> makes a easy and funny way,<br /> like receiving a cup of coffee from people loving you.
+                            </Typography>
+                        </m.div>
+                        
                     </Stack>
                 </Paper>
             </Grid>
             <Grid item xs={12}>
                 <Paper sx={{ pt: 10, pb: 20, bgcolor: 'white' }}>
                     <Stack spacing={5} alignItems={'center'}>
-                        <Typography px={15} variant="h1" fontWeight={'700'}>
-                            Decentralized Tipping Platform for Creators
-                        </Typography>
+                        <m.div variants={varFade().inUp}>
+                            <Typography px={15} variant="h2" fontWeight={'700'}>
+                                Decentralized Tipping Platform for Creators
+                            </Typography>
+                        </m.div>
                         <Stack direction={'row'} gap={3} justifyItems={'center'}>
                             <Stack gap={1} alignItems={'center'}>
-                                <Typography variant="h3">Motivate contributions</Typography>
-                                <Typography sx={{ px: 5 }} fontSize={24}>Increase motivation for Web3 community with token of SUI ecosystem</Typography>
+                                <m.div variants={varFade().inDown}>
+                                    <Typography variant="h5">Motivate contributions</Typography>
+                                    <Typography sx={{ px: 5 }} fontSize={18}>Increase motivation for Web3 community with token of SUI ecosystem</Typography>
+                                </m.div>
                             </Stack>
                             <Stack gap={1} alignItems={'center'}>
-                                <Typography variant="h3" alignItems={'center'}>Ease of Use</Typography>
-                                <Typography sx={{ px: 5 }} fontSize={24}>Creators generate links/QR codes; supporters donate with a click (Web & Mobile)</Typography>
+                                <m.div variants={varFade().inDown}>
+                                    <Typography variant="h5" alignItems={'center'}>Ease of Use</Typography>
+                                    <Typography sx={{ px: 5 }} fontSize={18}>Creators generate links/QR codes; supporters donate with a click (Web & Mobile)</Typography>
+                                </m.div>
                             </Stack>
                             <Stack gap={1} alignItems={'center'}>
-                                <Typography variant="h3" alignItems={'center'}>Wordpress plugin</Typography>
-                                <Typography sx={{ px: 5 }} fontSize={24}>Receive support from millions of users on wordpress</Typography>
+                                <m.div variants={varFade().inDown}>
+                                    <Typography variant="h5" alignItems={'center'}>Wordpress plugin</Typography>
+                                    <Typography sx={{ px: 5 }} fontSize={18}>Receive support from millions of users on wordpress</Typography>
+                                </m.div>
                             </Stack>
                         </Stack>
                     </Stack>
@@ -103,10 +120,11 @@ export default function LandingPage()
     return (
         <Page title="Landing Page">
             <RootStyle>
-                <Container maxWidth={themeStretch ? false : 'lg'} sx={{
+                <Container component={MotionViewport} maxWidth={themeStretch ? false : 'lg'} sx={{
                     textAlign: 'center',
                     alignItems: 'center',
                     justifyContent: 'space-between',
+                    mt: '71.5px',
                 }} >
                     <LadingPageComponent />
                 </Container>
